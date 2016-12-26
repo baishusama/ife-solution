eeba1ae414423e8c148ba9d83415b9d3aa15dacc
baishusama <baishusama@outlook.com>
2016-12-26 17:39:55 +0800

    task18 almost finished.

--- 2016-Spring/Task18@s2/task18.js
+++ 2016-Spring/Task18@s2/task18.js
@@ -20,6 +20,10 @@ function inputIsNum() {
     return false;
 }
 
+/* 利用事件委托，避开循环绑定事件 */
+
+// 编写 html 时为 input[button] 添加的 id 包含按钮将触发事件的方向（direction）和增删（operate）信息，
+// 以此结合数组的 push/pop/unshift/shift 方法来模拟队列的操作
 btns.addEventListener("click", function(e) {
     if (e.target.nodeName === "INPUT") {
         var btnId = e.target.id;
@@ -56,6 +60,8 @@ btns.addEventListener("click", function(e) {
     }
 }, false);
 
+// 生成 li 时添加的类名 index-$ 包含在 queue （数组类型）中的位置信息，
+// 以此结合数组的 filter 方法来更新 queue
 ul.addEventListener("click", function(e) {
     if (e.target.nodeName === "LI") {
         var i = parseInt(e.target.className.split("-")[1]);
