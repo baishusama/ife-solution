358801c491b88d1490f43b32cb191b5f1d8df5f7
dingxinyi <baishusama@outlook.com>
2017-01-03 01:01:27 +0800

    trying bubble-sort

--- 2016-Spring/task19@s2_visualize_bubble_sort_algorithm/task19.js
+++ 2016-Spring/task19@s2_visualize_bubble_sort_algorithm/task19.js
@@ -3,7 +3,7 @@ var input = document.getElementById("queue-input");
 var btns = document.getElementsByClassName("btns")[0];
 
 function isInputNum(rawInput) {
-    var trimmedInput = rawInput.trim() || input.value.trim();// ???
+    var trimmedInput = rawInput.trim() || input.value.trim(); // ???
     if (/^\d+$/.test(trimmedInput)) {
         return trimmedInput;
     }
@@ -12,7 +12,7 @@ function isInputNum(rawInput) {
 }
 
 function isInputInRange(numStr) {
-    var num = parseInt(numStr) || parseInt(input.value.trim());// ???
+    var num = parseInt(numStr) || parseInt(input.value.trim()); // ???
     // case1. parseInt(null) => NaN 会导致选择上式右侧
     if (num >= 10 && num <= 100) {
         return num;
@@ -20,14 +20,16 @@ function isInputInRange(numStr) {
     throw new Error("请输入 10 - 100 之间的数字~");
 }
 
-function validInput(rawInput){
+function validInput(rawInput) {
     return isInputInRange(isInputNum(rawInput));
 }
 
 var queue = (function() {
     // 利用 JS 的 Array 类型模拟队列
     var data = [];
-    
+    var status = [];
+    var STATUS = ["", "current", "ordered"]
+
     // ！？以下两个函数如果也返回的话，无法被返回的公有函数调用OAO
     // 不过，这两个函数目前仅被公有函数使用，令成私有似乎合情合理。。
     var isQueueEmpty = function() {
@@ -42,54 +44,74 @@ var queue = (function() {
         }
         throw new Error("队列已达到 60 上限，请在添加元素之前删除元素~");
     };
-    
+    var setStatus = function(index, status) {
+        status[index] = status;
+    };
+    this.drawQueue = function() {
+        var res = "";
+        data.map(function(elem, i) {
+            var clsName = STATUS[status[i]];
+            res += '<li style="height:' + elem + 'px;" title="' + elem + '" class="' + clsName + '"></li>';
+        });
+        ul.innerHTML = res;
+    }
     return {
-        toArray: function(){
+        toArray: function() {
             return data;
-        },// for test
-        drawQueue: function() {
+        }, // for test
+        'drawQueue': drawQueue,/*function() {
             var res = "";
             data.map(function(elem, i) {
                 res += '<li style="height:' + elem + 'px;" title="' + elem + '"></li>';
             });
             ul.innerHTML = res;
-        },
-        leftIn: function(value){
+        },*/
+        leftIn: function(value) {
             isQueueFull();
             data.unshift(validInput(value));
             return this;
         },
-        rightIn: function(value){
+        rightIn: function(value) {
             isQueueFull();
             data.push(validInput(value));
             return this;
         },
-        leftOut: function(){
+        leftOut: function() {
             isQueueEmpty();
-            alert("你去掉了左边的" + data.shift() + "~");
+            console.log("你去掉了左边的" + data.shift() + "~");
             return this;
         },
-        rightOut: function(){
+        rightOut: function() {
             isQueueEmpty();
-            alert("你去掉了右边的" + data.pop() + "~");
+            console.log("你去掉了右边的" + data.pop() + "~");
             return this;
         },
-        deleteElemAt: function(index){
+        deleteElemAt: function(index) {
             isQueueEmpty();
             data.splice(index, 1);
-            return this;// 可以来一波 链式写法 里么 www ！
+            return this; // 可以来一波 链式写法 里么 www ！
         },
         generateRandomQueue: function(length) {
             for (var i = 0; i < length; i++) {
                 data.push(Math.floor(Math.random() * (100 - 10 + 1) + 10));
+                status.push(0);
             }
             return this;
         },
         disruptQueue: function() {
             // 这里用啥算法比较好捏。。？哈希表？
         },
-        sortByBubble: function(){
-
+        bubbleSort: function() {
+            // var round = 1;
+            for (var round = 1; round < data.length; round++) {
+                for (var i = 0; i < data.length - round; i++) {
+                    status = status.map(function(){ return 0; });
+                    setStatus(i, 1);
+                    setStatus(i + 1, 1);
+                    drawQueue();
+                }
+            }
+            return this;
         }
     };
 })();
@@ -101,14 +123,16 @@ var queue = (function() {
 btns.addEventListener("click", function(e) {
     if (e.target.nodeName === "INPUT") {
         var funcName = e.target.id
-                        //.replace("-i",'I').replace("-o",'O');// 这里有更好的解决方法么？？？
-                        .replace(/\-[a-z]/, function(str){ return str[1].toUpperCase(); })
+            //.replace("-i",'I').replace("-o",'O');// 这里有更好的解决方法么？？？
+            .replace(/\-[a-z]/, function(str) {
+                return str[1].toUpperCase();
+            })
         var value = input.value;
         try {
             queue[funcName](value).
-                drawQueue();
+            drawQueue();
         } catch (e) {
-            alert(e.message);
+            console.log(e.message);
         }
         input.value = "";
     }
@@ -123,10 +147,11 @@ ul.addEventListener("click", function(e) {
             queue.deleteElemAt(index)
                 .drawQueue();
         } catch (e) {
-            alert(e.message);
+            console.log(e.message);
         }
     }
 }, false);
+
 
 
 window.onload = function() {
