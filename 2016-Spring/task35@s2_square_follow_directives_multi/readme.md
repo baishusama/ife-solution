52d6a655209294649d66cedfffa073369a5faee9
baishusama <baishusama@outlook.com>
2017-01-18 17:29:28 +0800

    task35's textarea autoSize and errorCheck is done.

--- 2016-Spring/task35@s2_square_follow_directives_multi/readme.md
+++ 2016-Spring/task35@s2_square_follow_directives_multi/readme.md
@@ -1,1 +1,41 @@
 [About IFE-2016-SP-task35]().
+
+## textarea 避开监听滚动事件来实现同步滚动行号
+
+### 思路
+
+在一个设置了 `overflow-y: scroll;` 的父级 `div` 中，正常情况下，两个同级的并排的 div 能够在父元素中，同步地上下滚动。
+
+所以我们需使 `textarea` 具有和 `div` 一样的特性——能够根据所包含的文字内容的高度，自动地变化自己的高度。
+
+#### textarea 高度自适应的问题 - 法一
+
+> **参考**
+> [Autosizing textarea using Prototype @SO](http://stackoverflow.com/questions/7477/autosizing-textarea-using-prototype) 提问中 Jan Miksovsky 的回答，以及回答下面的评论提出的一些修改意见。
+
+可以查看 [`jsfiddle` 上的在线 demo]()。
+
+这个解决方法 **棒** 在更多地依赖 HTML 和 CSS，而不是 JS 。
+
+但是也存在 **缺点** ：在最后一行进行回车的时候，高度会有波动，而不是直接一步到位。
+
+**原因** 在于，本例中，`textarea` 的高度是 `height: 100%;` 依赖于父元素的高度，而父元素的高度依赖于 `div#textCopy` 的高度。
+
+在最后一行进行回车的时候，`textarea` 的文字内容的最下方会增高一行，此时父元素高度不变，即 `textarea` 的高度不变，默认地 `textarea` 内容高度大于元素本身高度、第一行的内容将上移并被遮挡。而回车触发了 `keydown/keyup` 事件，回车被 JS 中定义的 `autoSize()` 转换成了 `<br/>` 并传给了 `div#textCopy` ，`div#textCopy` 的高度由此变高，父元素和 `textarea` 的高度也相应变高。第一行的内容也就由此能够避免被遮挡的命运了。
+
+#### textarea 高度自适应的问题 - 其它方法
+
+> 其它待读的相关资料：
+> [Creating a textarea with auto-resize](https://stackoverflow.com/questions/454202/creating-a-textarea-with-auto-resize)
+> 
+> 可以适当参考其它成熟实现：
+> [babeljs.io](https://babeljs.io/repl/)
+
+### 小结
+
+由于目前需要实现的并不是多么完备成熟的文本编辑器（需求详见 task35 任务说明），本次实现的文本编辑器还存在有待改进之处。
+
+比如：
+1. 没有考虑到，当输入一行很长的代码，且不换行的情况下，行号间应该存在空隔。
+2. 当输入行数很多的情况下，数字很大的行号的样式问题。
+3. 除了键盘输入外的其他输入方式（鼠标右键复制等）。
---
+++ 2016-Spring/task35@s2_square_follow_directives_multi/task35_v2.js
@@ -0,0 +1,386 @@
+// 解决 IE 和非 IE 浏览器在事件绑定方面的兼容性
+function addEventHandler(elem, event, handler) {
+    if (elem.addEventListener) {
+        elem.addEventListener(event, handler, false);
+    } else if (elem.attachEvent) {
+        elem.attachEvent("on" + event, handler);
+    } else {
+        elem["on" + event] = handler;
+    }
+}
+
+// 以下关于 css 前缀的处理方法借鉴自 jQuery ：jquery-3.1.0.js
+function getProperName(cssPropertyName) {
+    var name = cssPropertyName;
+    var cssPrefixes = ["Webkit", "Moz", "ms"];
+    var emptyStyle = document.createElement("div").style;
+    if (name in emptyStyle) {
+        return name;
+    }
+
+    var capName = name[0].toUpperCase + name.slice(1);
+    var i = cssPrefixes.length;
+    while (i--) {
+        name = cssPrefixes[i] + capName;
+        if (name in emptyStyle) {
+            return name;
+        }
+    }
+}
+
+function generateInlineStyle(degree) {
+    var deg = degree || 0;
+    return 'rotate(' + deg + 'deg)';
+}
+
+// 用于偷懒的 $ 方法
+function $(selector) {
+    var elem = null;
+    if (selector.indexOf('#') === 0) {
+        elem = document.getElementById(selector.slice(1));
+    } else if (selector.indexOf('.') === 0) {
+        elem = document.getElementsByClassName(selector.slice(1));
+    }
+    return elem;
+}
+
+// DOM
+// var squareDOM = document.getElementById("square");
+var boardDOM = $("#board");
+var directivesDOM = $("#directives");
+var runBtnDOM = $(".run")[0];
+var refreshBtnDOM = $(".refresh")[0];
+
+// 区域
+var board = (function() {
+    var ROW = 10;
+    var COLUMN = 10;
+    var bd = [];
+
+    var generateBoard = function() {
+        var cell = null;
+        for (var i = 0; i < ROW; i++) {
+            var rowTmp = [];
+            for (var j = 0; j < COLUMN; j++) {
+                cell = document.createElement("div");
+                cell.className = "grid";
+                if (j === 0) {
+                    cell.className += " clearfix";
+                } else if (j === 9) {
+                    cell.className += " rightmost";
+                }
+                if (i === 9) {
+                    cell.className += " downmost";
+                }
+                boardDOM.appendChild(cell);
+                rowTmp.push(0);
+            }
+            bd.push(rowTmp);
+        }
+    };
+
+    return {
+        generate: function() {
+            generateBoard();
+        },
+        getRowNum: function() {
+            return ROW;
+        },
+        getColNum: function() {
+            return COLUMN;
+        },
+        getBoardState: function() {
+            return bd;
+        }
+    };
+})();
+
+// 小方块
+var square = (function() {
+    var sqPos = {
+        x: 1,
+        y: 1
+    }; // default position // 1~10
+    var sqDir = 0; // default direction // ...,-3,-2,-1,0,1,2,3,...
+    var squareDOM = null;
+    var PROPERTY = getProperName("transform");
+    console.log("当前浏览器支持的是 " + PROPERTY);
+
+    // 真正的取模，而不是取余
+    var directionMod4 = function(direction) {
+        return (direction % 4 + 4) % 4;
+    };
+
+    // 对 squareDOM 进行移动（定位）
+    var moveSqDOM = function() {
+        squareDOM.style.top = (sqPos.x - 1) * 40 - 1 + "px";
+        squareDOM.style.left = (sqPos.y - 1) * 40 - 1 + "px";
+    };
+
+    // 返回小方块将抵达的位置
+    var traDir = function(direction, pace) {
+        var furPos = sqPos;
+        if (direction === 0 && furPos.x >= pace + 1) {
+            furPos.x -= pace;
+        } else if (direction === 1 && furPos.y <= board.getColNum() - pace) {
+            furPos.y += pace;
+        } else if (direction === 2 && furPos.x <= board.getRowNum() - pace) {
+            furPos.x += pace;
+        } else if (direction === 3 && furPos.y >= pace + 1) {
+            furPos.y -= pace;
+        } else {
+            throw new Error("将抵达边界，无法继续前进 XO");
+        }
+        return furPos;
+    };
+
+    // 尝试移动小方块，当“抵达边界”或“存在障碍物”时报错，否则返回 true
+    var tryMove = function(direction, pace) {
+        var pac = pace || 1;
+        var dir = (typeof direction === "number") ? direction : directionMod4(sqDir); // direction can be 0
+
+        var furPos = traDir(dir, pac);
+
+        if (board.getBoardState()[furPos.x - 1][furPos.y - 1] === 0) {
+            sqPos = furPos;
+        } else {
+            throw new Error("此方向存在障碍物，无法继续前进 XO");
+        }
+
+        return true;
+    };
+
+    // 返回转到某方向的最小角度
+    var turnTo = function(direction) {
+        var curDir = directionMod4(sqDir); // 小方块的当前朝向
+        // if(curDir === direction) return 0;
+
+        /* 就近转向 */
+        // Any better way ???
+        var cwStep = 0; // 顺时针（clockwise）所需转向次数
+        while (true) {
+            if ((curDir + cwStep) % 4 === direction) {
+                break;
+            }
+            cwStep++;
+        }
+
+        var deg = 0;
+        if (cwStep > 2) {
+            deg = (cwStep - 4) * 90;
+        } else {
+            deg = cwStep * 90;
+        }
+        return deg;
+    };
+
+    // 转向
+    var takeTurn = function(degree) {
+        var deg = degree;
+        sqDir += deg / 90;
+
+        /* 小方块的旋转 */
+        squareDOM.style[PROPERTY] = generateInlineStyle(90 * sqDir);
+    };
+
+    // 移动小方块
+    var move = function(direction, pace, isMOV) {
+        if (tryMove(direction, pace)) {
+            if (isMOV) { // 如果是 MOV 指令，在移动前，先转向
+                takeTurn(turnTo(direction));
+            }
+            moveSqDOM();
+        }
+    };
+
+    return {
+        generate: function() {
+            // get square random position and direction
+            sqPos.x = Math.ceil(Math.random() * 10);
+            sqPos.y = Math.ceil(Math.random() * 10);
+            sqDir = Math.floor(Math.random() * 4);
+
+            // generate dom
+            squareDOM = document.createElement("div");
+            squareDOM.id = "square";
+            moveSqDOM();
+            squareDOM.style[PROPERTY] = generateInlineStyle(90 * sqDir);
+            boardDOM.appendChild(squareDOM);
+        },
+        go: function() {
+            move();
+        },
+        tunlef: function() {
+            takeTurn(-90);
+        },
+        tunrig: function() {
+            takeTurn(90);
+        },
+        tunbac: function() {
+            takeTurn(180);
+        },
+        tratop: function() {
+            move(0);
+        },
+        trarig: function() {
+            move(1);
+        },
+        trabot: function() {
+            move(2);
+        },
+        tralef: function() {
+            move(3);
+        },
+        movtop: function() {
+            move(0, 1, true);
+        },
+        movrig: function() {
+            move(1, 1, true);
+        },
+        movbot: function() {
+            move(2, 1, true);
+        },
+        movlef: function() {
+            move(3, 1, true);
+        },
+    };
+})();
+
+// 代码编辑 textarea - Part 1
+/*(function() {
+    function autoSize() {
+        var lineH = directivesDOM.value.match(/\n/g).length;
+
+    }
+})();*/
+
+// 代码编辑 textarea - Part 2
+var codeArea = (function() {
+    // DOM: directivesDOM
+    var lineNumDOM = $("#lineNum");
+    var trmdVals = [];
+    var rows = 1;
+    var executable = 0; // nothing to execute
+    // var VALIDKEYCODE = []
+
+    function setLineNum(num) {
+        var i = 0;
+        while (lineNumDOM.children.length < num) {
+            lineNumDOM.appendChild(document.createElement("li"));
+        }
+        while (lineNumDOM.children.length > num) {
+            lineNumDOM.lastElementChild.remove();
+        }
+    }
+
+    function autoSize() {
+        var lineH = (directivesDOM.value.match(/\n/g) && directivesDOM.value.match(/\n/g).length + 1) || 1;
+        console.log("line should up to " + lineH)
+        setLineNum(lineH);
+    }
+
+    function getDirectives() {
+        var values = directivesDOM.value.split("\n"); //.trim().replace(/\s/g, '').toLowerCase();
+        console.log("line's num: " + values.length)
+
+        // 去掉空白符 & 小写
+        var trimmedValues = values.map(function(elem, index) {
+            return elem.trim().replace(/\s/g, '').toLowerCase();
+        });
+
+        return trimmedValues;
+    }
+
+    function findError() {
+        var errorLine = [];
+        var values = getDirectives();
+
+        for (var i = 0; i < values.length; i++) {
+            if (values[i] && !square[values[i]]) { // 非空行
+                errorLine.push(i);
+            }
+        }
+
+        return errorLine;
+    }
+
+    function highlightError(errors) {
+        for (var i = 0; i < lineNumDOM.children.length; i++) {
+            if (errors.indexOf(i) !== -1) {
+                lineNumDOM.children[i].className = "errorLine";
+            } else {
+                lineNumDOM.children[i].className = "";
+            }
+        }
+    }
+
+    function autoSizeAndCheck(e) {
+        // if (e.keyCode === 13) {
+        autoSize();
+        highlightError(findError()); // checkError();
+        // }
+    }
+
+    return {
+        initialize: function() {
+            addEventHandler(directivesDOM, "change", autoSizeAndCheck);
+            addEventHandler(directivesDOM, "keydown", autoSizeAndCheck);
+            addEventHandler(directivesDOM, "keyup", autoSizeAndCheck);
+            autoSize();
+        }
+    };
+})();
+
+var getValidDirective = function(rawValue) {
+    var values = rawValue.split("\n"); //.trim().replace(/\s/g, '').toLowerCase();
+    console.log("line's num: " + values.length)
+
+    // 去掉空白符 & 小写
+    var trimmedValues = values.map(function(elem, index) {
+        return elem.trim().replace(/\s/g, '').toLowerCase();
+    });
+
+    // 数组去重
+    var valueList = {};
+    var uniqueTrmdValues = [];
+    for (var i = 0; i < trimmedValues.length; i++) {
+        if (valueList[trimmedValues[i]] === undefined) {
+            valueList[trimmedValues[i]] = 1;
+            uniqueTrmdValues.push(trimmedValues[i]);
+        } /* else { valueList[trimmedValues[i]]++; }*/
+    }
+
+    uniqueTrmdValues.forEach(function(elem, index) {
+        // return
+    });
+    /*if (square[value]) {
+        return value;
+    }
+    throw new Error("非法指令，请重新尝试 :(\n合法的指令有如下：\n  GO\n  TUN LEF\n  TUN RIG\n  TUN BAC\n  TRA TOP\n  TRA RIG\n  TRA BOT\n  TRA LEF\n  MOV TOP\n  MOV RIG\n  MOV BOT\n  MOV LEF");*/
+};
+
+addEventHandler(runBtnDOM, "click", function() {
+    var rawValue = directivesDOM.value;
+    console.log(rawValue)
+    try {
+        getValidDirective(rawValue) // test
+            // square[getValidDirective(rawValue)]();
+    } catch (e) {
+        alert(e.message);
+    }
+});
+
+/*directivesDOM.onkeydown = function(event) {
+    var e = event || window.event;
+    switch (e.keyCode) {
+        case 13:
+            dirInputBtnDOM.click(); // 关于模拟点击事件触发的其他方法（better way???）
+            break;
+    }
+};*/
+
+window.onload = function() {
+    board.generate();
+    square.generate();
+    codeArea.initialize();
+};
